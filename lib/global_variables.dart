cf37f6477bba749c7e0c802f1cba12bcecb460b6
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-04-28 18:30:38 +0900

    updated global_variables.dart

---
+++ lib/global_variables.dart
@@ -0,0 +1,45 @@
+final products = [
+  {
+    'id': '0',
+    'title': 'Men\'s Nike Shoes',
+    'price': 44.52,
+    'sizes': [8, 9, 10, 11],
+    'company': 'Nike',
+    'imageUrl': 'images/1.png',
+  },
+  {
+    'id': '1',
+    'title': 'Adidas Shoes',
+    'price': 20.12,
+    'sizes': [9, 10, 12],
+    'company': 'Adidas',
+    'imageUrl': 'images/1.png',
+  },
+  {
+    'id': '2',
+    'title': 'Converse Shoes',
+    'price': 28.95,
+    'sizes': [8, 9, 10],
+    'company': 'Converse',
+    'imageUrl': 'images/1.png',
+  },
+  {
+    'id': '3',
+    'title': 'Jordan Shoes',
+    'price': 420.69,
+    'sizes': [8, 9, 10, 11],
+    'company': 'Nike',
+    'imageUrl': 'images/1.png',
+  },
+];
+
+final cart = [
+  {
+    'id': '3',
+    'title': 'Jordan Shoes',
+    'price': 420.69,
+    'sizes': 8,
+    'company': 'Nike',
+    'imageUrl': 'images/2.png',
+  }
+];
